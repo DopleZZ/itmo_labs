678d0392ff05a3aca99255a58dcf60e3f681169a
vodka_kota <109616806+DopleZZ@users.noreply.github.com>
2023-11-26 21:05:06 +0300

    Delete inf/task3.py

--- inf/task3.py
+++
@@ -1,1 +0,0 @@
-print(*sorted([__import__('re').findall(r'[а-яА-ЯёЁ]*',i)[0] for i in open('task3_test.txt').read().split() if len(set(__import__('re').findall(r'[аеёиоуыэюя]',i))) == 1], key=len), sep='\n')
